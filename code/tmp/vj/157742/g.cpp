70c8db8d9053e17dd5c79ecb2042015b960e786c
Shiyang <dr@shiyang.me>
2017-04-13 21:31:48 +0800

    update

---
+++ code/tmp/vj/157742/g.cpp
@@ -0,0 +1,15 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  for (int i = 2; i <= n; i++) {
+    
+  }
+  return 0;
+}
