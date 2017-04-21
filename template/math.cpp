721ce041be3a667d2a655673c9aa57aa4c9bb605
shi-yang <dr@shiyang.me>
2017-04-21 19:30:08 +0800

    update

--- template/math.cpp
+++ template/math.cpp
@@ -3,22 +3,21 @@
 //最大公约数
 int gcd(int a, int b)
 {
-	return b ? gcd(b, a % b) : a;
+	return b == 0 ? a : gcd(b, a % b);
 }
 
-//素数打表
+//素数打表O(nlog(log(n)))
 const int maxn = 1000;
 bool primes[maxn];
 void get_primes()
 {
 	memset(primes, true, sizeof(primes));
-	for (int i = 2; i <= sqrt(n + 0.5); i++) {
+	for (int i = 2; i <= maxn; i++)
 		if (primes[i]) {
-			for (int j = i * i; j <= n; j += i) {
+			for (int j = i * i; j <= maxn; j++) {
 				primes[j] = false;
 			}
 		}
-	}
 }
 
 
@@ -49,4 +48,111 @@ void ex_gcd(int a, int b, int &d, int &x, int &y)
 		gcd(b, a % b, d, y, x);
 		y -= x * (a / b);
 	}
+}
+
+
+typedef long long ll;
+// a * b % c
+inline ll mul(ll a, ll b, ll c)
+{
+	ll res = 0;
+	a %= c, b %= c;
+	while (b) {
+		if (b & 1) {
+			res += a;
+			if (res >= c) res -= c;
+		}
+		a <<= 1;
+		if (a >= c)
+			a -= c;
+		b >>= 1;
+	}
+	return res;
+}
+// a ^ b % c
+inline ll power(ll n, ll k, ll m)
+{
+	ll res = 1;
+	while (k) {
+		if (k & 1)
+			res = mul(res, n, m);
+		n = mul(n, n, m);
+		k >>= 1;
+	}
+	return res;
+}
+bool check(ll a, ll x, ll n, ll t)
+{
+	ll res = power(a, x, n);
+	ll last = res;
+	while (t--) {
+		res = mul(res, res, n);
+		if (res == 1 && last != 1 && last != n - 1)
+			return true;
+		last =res;
+	}
+	if (res != 1)
+		return true;
+	return false;
+}
+// 判断素数
+bool miller_rabin(ll n, ll s)
+{
+	if (n < 2)	return false;
+	if (n == 2)	return true;
+	if ((n & 1) == 0)	return false;
+	ll x = n - 1;
+	ll t = 0;
+	while ((x & 1) == 0) {
+		t++, x >>= 1;
+	}
+	while (s--) {
+		ll a = rand() % (n - 1) + 1;
+		if (check(a, x, n, t))
+			return false;
+	}
+	return true;
+}
+
+ll factor[10000];
+int cnt;
+ll gcd(ll a, ll b)
+{
+	if (a == 0)	return 1;
+	if (a < 0)	return gcd(-a, b);
+	while (b) {
+		ll t = a % b;
+		a = b, b = t;
+	}
+	return a;
+}
+// 质因数分解
+ll pollard_rho(ll n, ll c)
+{
+	ll i = 1, k = 2;
+	ll x = rand() % n;
+	ll y = x;
+	while (1) {
+		i++;
+		x = (mul(x, x, n) + c) % n;
+		ll d = gcd(y - x, n);
+		if (d != 1 && d != n)	return d;
+		if (y == x)	return n;
+		if (i == k) {
+			y = x;
+			k <<= 1;
+		}
+	}
+}
+void findfac(ll n)
+{
+	if (miller_rabin(n, 20)) {
+		factor[cnt++] = n;
+		return ;
+	}
+	ll p = n;
+	while (p >= n)
+		p = pollard_rho(p, rand() % (n - 1) + 1);
+	findfac(p);
+	findfac(n / p);
 }
